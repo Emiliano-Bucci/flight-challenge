86caedeabf204d476eca6af84107413ca13cfdfb
Emiliano-Bucci <emiliano.bucci@outlook.com>
2023-04-04 00:24:06 +0200

    feat: add docker to client project

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
       exportAsDefault: true,
     }),
   ],
+  server: {
+    port: 3030,
+    host: true,
+  },
+  preview: {
+    port: 3000,
+    host: true,
+  },
 });
